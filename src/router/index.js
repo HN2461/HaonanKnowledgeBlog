ee1507dded4f15436a8ea28431520573a676422b
陈浩南 <3519463440@qq.com>
2025-11-16 14:56:31 +0800

    配置GitHub Pages部署

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import NoteListPage from '../views/NoteListPage.vue'
 import NoteDetailPage from '../views/NoteDetailPage.vue'
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
